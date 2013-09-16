d1581afcd1917bfb51235b40494fd5d0118b251e
Roger Pettett <roger.pettett@nanoporetech.com>
2013-09-16 12:02:53 +0000

    merged pre-3.1

--- test/10-metrichor.js
+++ test/10-metrichor.js
@@ -120,5 +120,40 @@ describe('Array', function(){
 	    });
 	});
 
+	it('should list workflow_instances', function() {
+	    extRequestStub.get = function(obj, cb) {
+		assert.equal(obj.uri, "http://metrichor.local:8080/workflow_instance.js?apikey=FooBar02");
+		cb(null, null, '{"workflow_instances":[{"id_workflow_instance":"149","state":"running","workflow_filename":"DNA_Sequencing.js","start_requested_date":"2013-09-16 09:25:15","stop_requested_date":"2013-09-16 09:26:04","start_date":"2013-09-16 09:25:17","stop_date":"2013-09-16 09:26:11","control_url":"127.0.0.1:8001","data_url":"localhost:3006"}]}');
+	    };
+
+	    var Client = new metrichor({
+		"url"    : "http://metrichor.local:8080",
+		"apikey" : "FooBar02"
+	    });
+
+	    Client.workflow_instances(function(err, obj) {
+		assert.equal(err, null, 'no error reported');
+		assert.deepEqual(obj, [{"id_workflow_instance":"149","state":"running","workflow_filename":"DNA_Sequencing.js","start_requested_date":"2013-09-16 09:25:15","stop_requested_date":"2013-09-16 09:26:04","start_date":"2013-09-16 09:25:17","stop_date":"2013-09-16 09:26:11","control_url":"127.0.0.1:8001","data_url":"localhost:3006"}], 'workflow instance list');
+	    });
+	});
+
+	it('should read a workflow_instance', function() {
+	    extRequestStub.get = function(obj, cb) {
+		assert.equal(obj.uri, "http://metrichor.local:8080/workflow_instance/149.js?apikey=FooBar02");
+		cb(null, null, '{"id_workflow_instance":"149","state":"running","workflow_filename":"DNA_Sequencing.js","start_requested_date":"2013-09-16 09:25:15","stop_requested_date":"2013-09-16 09:26:04","start_date":"2013-09-16 09:25:17","stop_date":"2013-09-16 09:26:11","control_url":"127.0.0.1:8001","data_url":"localhost:3006"}');
+	    };
+
+	    var Client = new metrichor({
+		"url"    : "http://metrichor.local:8080",
+		"apikey" : "FooBar02"
+	    });
+
+	    Client.workflow_instance(149, function(err, obj) {
+		assert.equal(err, null, 'no error reported');
+		assert.deepEqual(obj, {"id_workflow_instance":"149","state":"running","workflow_filename":"DNA_Sequencing.js","start_requested_date":"2013-09-16 09:25:15","stop_requested_date":"2013-09-16 09:26:04","start_date":"2013-09-16 09:25:17","stop_date":"2013-09-16 09:26:11","control_url":"127.0.0.1:8001","data_url":"localhost:3006"}, 'workflow read');
+	    });
+	});
+
+
     });
 });
