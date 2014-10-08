700e5373af149bbc9ee11e78c87b9a9f0204b98b
Roger Pettett <roger.pettett@nanoporetech.com>
2014-10-08 16:08:16 +0000

    added jslint tests; many jslint fixes

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 deps:
-	npm install mocha proxyquire request xunit-file
+	npm install
+
+test_common:
+	node_modules/.bin/jslint metrichor.js
 
 test: deps
+	make test_common
 	@./node_modules/.bin/mocha --reporter xunit-file
 
-just_test:
+just_test: test_common
 	@./node_modules/.bin/mocha
--- metrichor.js
+++ metrichor.js
@@ -13,233 +13,237 @@
  *   rejectUnauthorized: false,
  *   requestCert: true,
  *   agent: false,
- *   proxy: "https://myproxy.com:3128/"
+ *   proxy: "http://myproxy.com:3128/"
  */
+/*jslint nomen: true*/
 
-module.exports = metrichor;
-module.exports.version = '1.0.0';
+"use strict";
 
 var fs         = require('fs');
 var extRequest = require('request');
 
-function metrichor (opt_string) {
-    this.className = 'metrichor';
+function metrichor(opt_string) {
     opt_string = opt_string || '{}';
     var opts;
 
-    if ( typeof opt_string === 'string' ||
-	 (typeof opt_string === "object" &&
-          opt_string.constructor === String) ) {
-	opts = JSON.parse(opt_string);
+    if (typeof opt_string === 'string' ||
+            (typeof opt_string === "object" &&
+            opt_string.constructor === String)) {
+        opts = JSON.parse(opt_string);
     } else {
-	opts = opt_string;
+        opts = opt_string;
     }
 
-    this._url           = opts.url ? opts.url : 'https://metrichor.com';
+    this._url           = opts.url || 'https://metrichor.com';
     this._apikey        = opts.apikey;
     this._proxy         = opts.proxy;
     this._agent_version = opts.agent_version;
 
     return this;
-};
+}
+
+module.exports = metrichor;
+module.exports.version = '0.4.0';
 
 metrichor.prototype = {
-    _accessor : function(field, value) {
-	var store = '_'+field;
-	if ( typeof value !== 'undefined' ) {
-	    this[store] = value;
-	}
-	return this[store];
+    _accessor : function (field, value) {
+        var store = '_' + field;
+        if (value !== undefined) {
+            this[store] = value;
+        }
+        return this[store];
     },
 
-    url : function(url) {
-	return this._accessor('url', url);
+    url : function (url) {
+        return this._accessor('url', url);
     },
 
-    apikey : function(apikey) {
-	return this._accessor('apikey', apikey);
+    apikey : function (apikey) {
+        return this._accessor('apikey', apikey);
     },
 
-    user : function(cb) {
-
-	return this._get('user', cb);
-    },
-  
-    workflows : function(cb) {
-
-	return this._list('workflow', cb);
+    user : function (cb) {
+        return this._get('user', cb);
     },
 
-    workflow  : function(id, obj, cb) {
-
-	if(cb == null) {
-	    // two args: get object
-	    cb=obj;
-	    return this._read('workflow', id, cb);
-
-	} else {
-	    // three args: update object
-	    return this._post('workflow', id, obj, cb);
-	}
+    workflows : function (cb) {
+        return this._list('workflow', cb);
     },
 
-    start_workflow : function(workflow_id, cb) {
+    workflow : function (id, obj, cb) {
 
-	return this._post('workflow_instance', null, { "workflow": workflow_id }, cb);
+        if (cb === null) {
+            // two args: get object
+            cb = obj;
+            return this._read('workflow', id, cb);
+        }
+
+        // three args: update object
+        return this._post('workflow', id, obj, cb);
     },
 
-    stop_workflow : function(instance_id, cb) {
-    
-	return this._put('workflow_instance/stop', instance_id, cb);
+    start_workflow : function (workflow_id, cb) {
+        return this._post('workflow_instance', null, { "workflow": workflow_id }, cb);
     },
-  
-    workflow_instances : function(cb) {
-    
-	return this._list('workflow_instance', cb);
-    },
-  
-    workflow_instance : function(id, cb) {
-    
-	return this._read('workflow_instance', id, cb);
-    },
-    
-    telemetry : function(id_workflow_instance, obj, cb) {
-    
-	if(cb == null) {
-	    // two args: get object
-	    cb=obj;
-	    return this._read('workflow_instance/telemetry', id_workflow_instance, cb);
-      
-	} else {
-	    // three args: update object
-	    return this._post('workflow_instance/telemetry', id_workflow_instance, obj, cb);
-	}
-    },
-  
-    _list : function ( entity, cb ) {
 
-	return this._get( entity, function(e, json) {
-	    cb(e, json[entity+"s"]);
-	});
+    stop_workflow : function (instance_id, cb) {
+        return this._put('workflow_instance/stop', instance_id, cb);
     },
-  
-    _read : function ( entity, id, cb ) {
-    
-	return this._get ( entity + '/' + id, cb );
+
+    workflow_instances : function (cb) {
+        return this._list('workflow_instance', cb);
+    },
+
+    workflow_instance : function (id, cb) {
+        return this._read('workflow_instance', id, cb);
+    },
+
+    telemetry : function (id_workflow_instance, obj, cb) {
+        if (cb === null) {
+            // two args: get object
+            cb = obj;
+            return this._read('workflow_instance/telemetry', id_workflow_instance, cb);
+        }
+
+        // three args: update object
+        return this._post('workflow_instance/telemetry', id_workflow_instance, obj, cb);
+    },
+
+    _list : function (entity, cb) {
+        return this._get(entity, function (e, json) {
+            cb(e, json[entity + "s"]);
+        });
+    },
+
+    _read : function (entity, id, cb) {
+        return this._get(entity + '/' + id, cb);
     },
 
     _get : function (uri, cb) {
-	// do something to get/set data in metrichor
-	var srv  = this.url();
-	srv      = srv.replace(/\/+$/, ""); // clip trailing /s
-	var uri  = "/" + uri + ".js?apikey=" + this.apikey();
-	uri      = uri.replace(/\/+/g, "/");
+        // do something to get/set data in metrichor
+        var call, mc,
+            srv = this.url();
 
-        if(this._agent_version) {
+        uri = "/" + uri + ".js?apikey=" + this.apikey();
+        srv = srv.replace(/\/+$/, ""); // clip trailing /s
+        uri = uri.replace(/\/+/g, "/");
+
+        if (this._agent_version) {
             uri = uri + ";agent_version=" + this._agent_version;
         }
 
-	var call = srv + uri;
-	var mc   = this;
-    
+        call = srv + uri;
+        mc   = this;
 
-	extRequest.get(
-	    {
-		uri   : call,
-		proxy : this._proxy
-	    },
-	    function (e,r,body) { mc._responsehandler( e,r,body,cb ) }
-	);
+        extRequest.get(
+            {
+                uri   : call,
+                proxy : this._proxy
+            },
+            function (e, r, body) {
+                mc._responsehandler(e, r, body, cb);
+            }
+        );
     },
-  
-    _post : function(uri, id, obj, cb) {
-	var form = {
-	    'apikey' : this.apikey(),
-	    'json'   : JSON.stringify(obj)
-	};
 
-        if(this._agent_version) {
+    _post : function (uri, id, obj, cb) {
+        var srv, call, mc,
+            form = {
+                apikey: this.apikey(),
+                json:   JSON.stringify(obj)
+            };
+
+        if (this._agent_version) {
             form.agent_version = this._agent_version;
         }
 
-	/* if id is an object, merge it into form post parameters */
-	if(typeof id === 'object') {
-	    for (var attr in id) {
-		form[attr] = id[attr];
-	    }
-	    id="";
-	}
-      
-	var srv  = this.url();
-	srv      = srv.replace(/\/+$/, ""); // clip trailing /s
-	uri      = uri.replace(/\/+/g, "/");
-	var call = srv + '/' + uri;
-      
-	if ( id ) {
-	    call = call + '/' + id;
-	}
-	call += '.js';
-      
-	var mc   = this;
-      
-	extRequest.post(
-	    {
-		uri   : call,
-		form  : form,
-		proxy : this._proxy
-	    },
-	    function ( e,r,body ){ mc._responsehandler( e,r,body,cb ) }
-	);
+        /* if id is an object, merge it into form post parameters */
+        if (id && typeof id === 'object') {
+            Object.keys(id).forEach(function (attr) {
+                form[attr] = id[attr];
+            });
+
+            id = "";
+        }
+
+        srv  = this.url();
+        srv  = srv.replace(/\/+$/, ""); // clip trailing /s
+        uri  = uri.replace(/\/+/g, "/");
+        call = srv + '/' + uri;
+        mc   = this;
+
+        if (id) {
+            call = call + '/' + id;
+        }
+        call += '.js';
+
+        extRequest.post(
+            {
+                uri   : call,
+                form  : form,
+                proxy : this._proxy
+            },
+            function (e, r, body) {
+                mc._responsehandler(e, r, body, cb);
+            }
+        );
     },
 
-    _put : function(uri, id, obj, cb) {
+    _put : function (uri, id, obj, cb) {
+        /* three-arg _put call (no parameters) */
+        if (typeof obj === 'function') {
+            cb = obj;
+        }
 
-	/* three-arg _put call (no parameters) */
-	if(typeof obj === 'function') {
-	    cb = obj;
-	}
-    
-	var form = {
-	    'apikey'        : this.apikey(),
-	    'json'          : JSON.stringify(obj)
-	};
+        var srv, call, mc,
+            form = {
+                apikey: this.apikey(),
+                json:   JSON.stringify(obj)
+            };
 
-        if(this._agent_version) {
+        if (this._agent_version) {
             form.agent_version = this._agent_version;
         }
 
-	var srv  = this.url();
-	srv      = srv.replace(/\/+$/, ""); // clip trailing /s
-	uri      = uri.replace(/\/+/g, "/");
-	var call = srv + '/' + uri + '/' + id + '.js';
-	var mc   = this;
-	extRequest.put(
-	    {
-		uri   : call,
-		form  : form,
-		proxy : this._proxy
-	    },
-	    function ( e,r,body ){ mc._responsehandler( e,r,body,cb ) }
-	);
+        srv  = this.url();
+        srv  = srv.replace(/\/+$/, ""); // clip trailing /s
+        uri  = uri.replace(/\/+/g, "/");
+        call = srv + '/' + uri + '/' + id + '.js';
+        mc   = this;
+
+        extRequest.put(
+            {
+                uri   : call,
+                form  : form,
+                proxy : this._proxy
+            },
+            function (e, r, body) {
+                mc._responsehandler(e, r, body, cb);
+            }
+        );
     },
-  
-    _responsehandler : function(res_e, r, body, cb) {
-	if ( res_e ) {
-	    return cb( res_e, {} );
-	}
 
-	var json;
-	try {
-	    json=JSON.parse( body );
+    _responsehandler : function (res_e, r, body, cb) {
+        if (res_e) {
+            return cb(res_e, {});
+        }
 
-	} catch(jsn_e) {
-	    return cb( jsn_e, {} );
-	}
-    
-	if ( json.error ) {
-	    return cb({"error": json.error}, {});
-	}
-    
-	return cb(null, json);
+        if (r && r.statusCode >= 400) {
+            return cb({"error": "HTTP status " + r.statusCode}, {});
+        }
+
+        var json;
+        try {
+            json = JSON.parse(body);
+
+        } catch (jsn_e) {
+            return cb(jsn_e, {});
+        }
+
+        if (json.error) {
+            return cb({"error": json.error}, {});
+        }
+
+        return cb(null, json);
     }
 };
