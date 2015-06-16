6cb1b11a443b1cade8094b47d414fcedb6e2b9c3
Roger Pettett <roger.pettett@nanoporetech.com>
2015-06-16 14:14:03 +0100

    part-implemented downloader

--- examples/folderWatch.js
+++ examples/folderWatch.js
@@ -1,11 +1,12 @@
 var Metrichor = require("../lib/metrichor");
 var m = new Metrichor({
-    watchFolder:          "/tmp",
+    watchFolder:          "/Users/rmp/whats-in-my-pot",
     id_workflow_instance: 61427,
     url:                  "https://dev.metrichor.com",
     apikey:               "27448a89ee9794f241307b7e8b9170ba0234b74e",
     bucket:               "eu-west-1-metrichor-dev",
-    queueName:            "000-chain-test-rmp-01",
+    inputQueueName:       "000-chain-test-rmp-01",
+    outputQueueName:      "000-chain-test-rmp-01",
     bucketFolder:         "rmp-test", // private outputqueue,
     targetComponentId:    "",
     chain: {
@@ -14,3 +15,19 @@ var m = new Metrichor({
 });
 
 m.autoConfigure();
+
+var statsInterval = setInterval(function () {
+    console.log("[" + (new Date()).toISOString() + "] stats", m.stats("sts_expiration"), m.stats("upload"), m.stats("download"));
+}, 5000);
+
+
+setTimeout(function () {
+    m.stop_everything();
+    clearInterval(statsInterval);
+}, 20000);
+
+process.on('SIGINT', function () {
+    console.log('Got SIGINT. Stopping.');
+    clearInterval(statsInterval);
+    m.stop_everything(function () { process.exit(); });
+});
