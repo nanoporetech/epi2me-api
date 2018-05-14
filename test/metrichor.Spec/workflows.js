d6e4aa8118a0adb97daeee7c2ecdbef9d5fd26f6
Roger Pettett <roger.pettett@nanoporetech.com>
2018-05-14 14:02:34 +0100

    repairing test coverage; poking proxyquire - finding tests which were not actually running

--- test/metrichor.Spec/workflows.js
+++ test/metrichor.Spec/workflows.js
@@ -1,49 +1,43 @@
 "use strict";
-const proxyquire     = require('proxyquire');
-const assert         = require("assert");
-const sinon          = require("sinon");
-const path           = require("path");
-const _              = require("lodash");
-const tmp            = require('tmp');
-const queue          = require('queue-async');
-const fs             = require('fs');
+const proxyquire   = require('proxyquire');
+const assert       = require("assert");
+const sinon        = require("sinon");
+const path         = require("path");
+const _            = require("lodash");
+const tmp          = require('tmp');
+const queue        = require('queue-async');
+const fs           = require('fs');
 let requestProxy   = {};
 let fsProxy        = {};
 let mkdirpProxy    = {};
 let awsProxy       = {};
-proxyquire('../../lib/utils', {
-    'request' : requestProxy
-});
-var EPI2ME = proxyquire('../../lib/metrichor.js', {
+let utilsProxy     = {};
+let EPI2ME         = proxyquire('../../lib/metrichor.js', {
     'aws-sdk'     : awsProxy,
     'graceful-fs' : fsProxy,
-    'mkdirp'      : mkdirpProxy
+    'mkdirp'      : mkdirpProxy,
+    './utils.js'       : utilsProxy,
 });
 
-describe('workflows', function(){
-
+describe('workflows', () => {
     it('should list workflows', () => {
-        var req,
-            client = new EPI2ME({
-                url    : "http://metrichor.local:8080",
-                apikey : "FooBar02",
-		agent_version: "0.18.12345"
-            });
+        let client = new EPI2ME({
+            url           : "http://metrichor.local:8080",
+            apikey        : "FooBar02",
+	    agent_version : "0.18.12345"
+        });
 
-        requestProxy.get = function(req, cb) {
-	    assert.deepEqual(req, {
-		uri: "http://metrichor.local:8080/workflow.js",
-		headers: {
-		    'X-EPI2ME-ApiKey':  'FooBar02',
-		    'X-EPI2ME-Client':  'Metrichor API',
-		    'X-EPI2ME-Version': '0.18.12345'
-		}});
-            cb(null, null, JSON.stringify({"workflows":[{"description":"a workflow"}]}));
-            delete requestProxy.get;
+        utilsProxy._get = (uri, options, cb) => {
+	    assert.equal(uri,                   "workflow");
+	    assert.equal(options.url,           "http://metrichor.local:8080");
+	    assert.equal(options.agent_version, '0.18.12345');
+	    assert.equal(options.apikey,        'FooBar02');
+	    assert.equal(options.user_agent,    'Metrichor API');
+            return cb(null, {workflows: [{"description":"a workflow"}]});
         };
 
         assert.doesNotThrow(() => {
-            client.workflows(function(e, o) {
+            client.workflows((e, o) => {
 		assert.equal(e,     null, 'no error reported');
 		assert.deepEqual(o, [{"description":"a workflow"}], 'workflow list');
             });
