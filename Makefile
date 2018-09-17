54bb854d8954adddcaecab60ee9d8972085fec7a
Roger Pettett <roger.pettett@nanoporetech.com>
2018-09-17 13:38:58 +0100

    rebuilt; added "make dist" target

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+dist:
+	yarn dist
+
 test:
 	yarn test
 
--- dist/rest.js
+++ dist/rest.js
@@ -44,7 +44,7 @@ class REST {
                 return;
             }
 
-            entity = entity.match(/^[a-z]+/i)[0]; // dataset?foo=bar => dataset
+            entity = entity.match(/^[a-z_]+/i)[0]; // dataset?foo=bar => dataset
             cb(null, json[entity + "s"]);
         });
     }
