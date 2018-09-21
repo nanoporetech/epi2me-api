10e0a9b567a395023bd73da7fb2770d13f1ea139
Roger Pettett <roger.pettett@nanoporetech.com>
2018-09-21 17:53:02 +0100

    coverage for _list

--- test/rest.Spec/_list.js
+++ test/rest.Spec/_list.js
@@ -26,13 +26,32 @@ describe('rest.workflows', () => {
 	stub.restore();
     });
 
-    it("must catch request failure", () => {
+    it("must catch request failure with structured error", () => {
 	let ringbuf    = new bunyan.RingBuffer({ limit: 100 });
         let log        = bunyan.createLogger({ name: "log", stream: ringbuf });
 	let stub = sinon.stub(utils, "_get").callsFake((uri, options, cb) => {
 	    assert.deepEqual(options, { log: log });
 	    assert.equal(uri, "thing", "url passed");
 	    cb({error: "request failure"});
+	});
+
+	let fake = sinon.fake();
+	let rest = new REST({log: log});
+	assert.doesNotThrow(() => {
+	    rest._list("thing", fake);
+	});
+	assert(fake.calledOnce, "callback invoked");
+	sinon.assert.calledWith(fake, "request failure");
+	stub.restore();
+    });
+
+    it("must catch request failure with unstructured", () => {
+	let ringbuf    = new bunyan.RingBuffer({ limit: 100 });
+        let log        = bunyan.createLogger({ name: "log", stream: ringbuf });
+	let stub = sinon.stub(utils, "_get").callsFake((uri, options, cb) => {
+	    assert.deepEqual(options, { log: log });
+	    assert.equal(uri, "thing", "url passed");
+	    cb("request failure");
 	});
 
 	let fake = sinon.fake();
