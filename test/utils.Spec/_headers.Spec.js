4c0c6db4dd3c1516cfc733d95a7d02b03a31c3a1
Roger Pettett <roger.pettett@nanoporetech.com>
2018-10-02 17:55:58 +0100

    coverage for _sign and _headers

---
+++ test/utils.Spec/_headers.Spec.js
@@ -0,0 +1,65 @@
+import utils from "../../lib/utils";
+
+const assert = require("assert");
+const sinon  = require("sinon");
+
+describe('utils._headers', () => {
+    it("should create empty headers if none set", () => {
+	let req  = {};
+	let stub = sinon.stub(utils, "_sign").callsFake();
+	utils._headers(req, {user_agent: "EPI2ME Test", agent_version: "0.0.1"});
+	assert.deepEqual(req.headers, {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "X-EPI2ME-Client": "EPI2ME Test",
+	    "X-EPI2ME-Version": "0.0.1"
+	});
+	assert(stub.calledOnce);
+	stub.restore();
+    });
+
+    it("should propagate existing headers", () => {
+	let req  = {headers: {"accept-language":"mt"}};
+	let stub = sinon.stub(utils, "_sign").callsFake();
+	utils._headers(req, {user_agent: "EPI2ME Test", agent_version: "0.0.1"});
+	assert.deepEqual(req.headers, {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "X-EPI2ME-Client": "EPI2ME Test",
+	    "X-EPI2ME-Version": "0.0.1",
+	    "accept-language":"mt"
+	});
+	assert(stub.calledOnce);
+	stub.restore();
+    });
+
+    it("should initialise options", () => {
+	let req  = {headers: {"accept-language":"mt"}};
+	let stub = sinon.stub(utils, "_sign").callsFake();
+	utils._headers(req);
+	assert.deepEqual(req.headers, {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+	    "X-EPI2ME-Client": "",
+	    "X-EPI2ME-Version": "0",
+	    "accept-language":"mt"
+	});
+	assert(stub.calledOnce);
+	stub.restore();
+    });
+
+    it("should not sign if requested", () => {
+	let req  = {headers: {"accept-language":"mt"}};
+	let stub = sinon.stub(utils, "_sign").callsFake();
+	utils._headers(req, {_signing: false});
+	assert.deepEqual(req.headers, {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+	    "X-EPI2ME-Client": "",
+	    "X-EPI2ME-Version": "0",
+	    "accept-language":"mt"
+	});
+	assert(stub.notCalled);
+	stub.restore();
+    });
+});
