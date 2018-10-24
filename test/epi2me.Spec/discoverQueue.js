40fc1fb57f043f6f3ef9505eb365ef583a77126f
Roger Pettett <roger.pettett@nanoporetech.com>
2018-10-24 12:02:31 +0100

    high coverage for discoverQueue

--- test/epi2me.Spec/discoverQueue.js
+++ test/epi2me.Spec/discoverQueue.js
@@ -1,72 +1,136 @@
 "use strict";
-const proxyquire     = require('proxyquire');
+
 const assert         = require("assert");
 const sinon          = require("sinon");
 const path           = require("path");
-const _              = require("lodash");
-const tmp            = require('tmp');
-const queue          = require('queue-async');
 
-let requestProxy   = {};
-let fsProxy        = {};
-let awsProxy       = {};
-proxyquire('../../lib/utils', {
-    'request' : requestProxy
-});
-var EPI2ME;
+import EPI2ME from "../../lib/epi2me";
 
-describe('Array', () => {
+describe('epi2me', () => {
 
-    beforeEach(() => {
-        EPI2ME = proxyquire('../../lib/epi2me', {
-            'aws-sdk'     : awsProxy,
-            'fs-extra' : fsProxy,
-        }).default;
-    });
+    describe('discoverQueue', () => {
 
-    describe('metrichor api', function(){
+        it('discovers successfully', () => {
+            let client = new EPI2ME({});
+            sinon.stub(client.log, "warn");
+            sinon.stub(client.log, "error");
+            sinon.stub(client.log, "info");
+            sinon.stub(client.log, "debug");
 
-        describe('.discoverQueue method', () => {
-            var client, queueUrl = 'queueUrl';
-            beforeEach(() => {
-                client = new EPI2ME({});
-                sinon.stub(client.log, "warn");
-                sinon.stub(client.log, "error");
-                sinon.stub(client.log, "info");
-            });
+	    let sqs = {
+		getQueueUrl: sinon.fake((opts, callback) => {
+		    assert.equal(opts.QueueName, "my_queue", "sqs getqueueurl for expected queue");
+		    callback(null, {QueueUrl: "https://my.cloud/queues/my_queue"});
+		}),
+	    };
 
-            /*it('should return sqs queue', () => {
-             var sqs = {
-             getQueueUrl: function (opts, cb) {
-             cb("Error");
-             assert(client.log.warn.calledOnce);
-             cb(null, { QueueUrl: "result" });
-             assert(client.log.warn.calledOnce);
-             throw Error
-             }
-             };
-             var successCb = sinon.spy();
-             var faliureCb = sinon.spy();
-             client.discoverQueue(sqs, 'queueName', successCb, faliureCb);
-             assert.equal(successCb.firstCall.args[0], "result");
-             assert.equal(faliureCb.firstCall.args[0], "getqueueurl error");
-             assert.equal(faliureCb.lastCall.args[0], "getqueueurl exception");
-             client.discoverQueue(sqs, 'queueName', successCb, faliureCb);
-             });*/
+	    let success = sinon.fake();
+	    let failure = sinon.fake();
+	    assert.doesNotThrow(() => {
+		client.discoverQueue(sqs, 'my_queue', success, failure);
+	    });
+	    sinon.assert.calledOnce(success);
+	    sinon.assert.notCalled(failure);
+	    assert.equal(success.args[0][0], "https://my.cloud/queues/my_queue", "success callback fired with queue url");
+        });
 
-            it('should handle sessionedSQS errors', () => {
-                sinon.stub(client, "sessionedSQS");
-                var completeCb = sinon.spy();
-                client.queueLength(queueUrl, completeCb);
-                //assert(completeCb.calledTwice, 'call callback even for errors');
-                assert.equal(completeCb.firstCall.args[0], undefined);
-                //assert.equal(completeCb.secondCall.args[0], undefined);
-                assert(client.log.error.calledOnce);
-                assert.doesNotThrow(() => {
-                    client.queueLength(queueUrl);
-                    client.queueLength();
-                }, 'Error');
-            });
+        it('discovers with cache hit', () => {
+            let client = new EPI2ME({});
+	    client.config.instance._discoverQueueCache["my_queue"] = "https://my.cloud/queues/my_queue";
+            sinon.stub(client.log, "warn");
+            sinon.stub(client.log, "error");
+            sinon.stub(client.log, "info");
+            sinon.stub(client.log, "debug");
+
+	    let sqs = {
+		getQueueUrl: sinon.fake((opts, callback) => {
+		    assert.equal(opts.QueueName, "my_queue", "sqs getqueueurl for expected queue");
+		    callback(null, {QueueUrl: "https://my.cloud/queues/my_queue"});
+		}),
+	    };
+
+	    let success = sinon.fake();
+	    let failure = sinon.fake();
+	    assert.doesNotThrow(() => {
+		client.discoverQueue(sqs, 'my_queue', success, failure);
+	    });
+	    sinon.assert.notCalled(sqs.getQueueUrl); // no need to run a real query
+	    sinon.assert.calledOnce(success);
+	    sinon.assert.notCalled(failure);
+	    assert.equal(success.args[0][0], "https://my.cloud/queues/my_queue", "success callback fired with queue url");
+        });
+
+        it('discovers badly', () => {
+            let client = new EPI2ME({});
+            sinon.stub(client.log, "warn");
+            sinon.stub(client.log, "error");
+            sinon.stub(client.log, "info");
+            sinon.stub(client.log, "debug");
+
+	    let sqs = {
+		getQueueUrl: sinon.fake((opts, callback) => {
+		    assert.equal(opts.QueueName, "my_queue", "sqs getqueueurl for expected queue");
+		    callback(null, {xQueueUrl: "https://my.cloud/queues/my_queue"});
+		}),
+	    };
+
+	    let success = sinon.fake();
+	    let failure = sinon.fake();
+	    assert.doesNotThrow(() => {
+		client.discoverQueue(sqs, 'my_queue', success, failure);
+	    });
+	    sinon.assert.calledOnce(failure);
+	    sinon.assert.notCalled(success);
+	    assert.equal(failure.args[0][0], "getqueueurl error", "failure callback fired with message");
+        });
+
+        it('fails to discover', () => {
+            let client = new EPI2ME({});
+            sinon.stub(client.log, "warn");
+            sinon.stub(client.log, "error");
+            sinon.stub(client.log, "info");
+            sinon.stub(client.log, "debug");
+
+	    let sqs = {
+		getQueueUrl: sinon.fake((opts, callback) => {
+		    assert.equal(opts.QueueName, "my_queue", "sqs getqueueurl for expected queue");
+		    callback(new Error("no such queue"));
+		}),
+	    };
+
+	    let success = sinon.fake();
+	    let failure = sinon.fake();
+	    assert.doesNotThrow(() => {
+		client.discoverQueue(sqs, 'my_queue', success, failure);
+	    });
+	    sinon.assert.calledOnce(failure);
+	    sinon.assert.notCalled(success);
+	    assert.equal(failure.args[0][0], "getqueueurl error", "failure callback fired with message");
+        });
+
+        it('fails to discover with proxy set', () => {
+            let client = new EPI2ME({proxy:"https://my.proxy:3128/"});
+            let warn = sinon.stub(client.log, "warn");
+            sinon.stub(client.log, "error");
+            sinon.stub(client.log, "info");
+            sinon.stub(client.log, "debug");
+
+	    let sqs = {
+		getQueueUrl: sinon.fake((opts, callback) => {
+		    assert.equal(opts.QueueName, "my_queue", "sqs getqueueurl for expected queue");
+		    callback(new Error("Unexpected close tag"));
+		}),
+	    };
+
+	    let success = sinon.fake();
+	    let failure = sinon.fake();
+	    assert.doesNotThrow(() => {
+		client.discoverQueue(sqs, 'my_queue', success, failure);
+	    });
+	    sinon.assert.calledOnce(failure);
+	    sinon.assert.notCalled(success);
+	    assert.equal(failure.args[0][0], "getqueueurl error", "failure callback fired with message");
+	    assert.ok(warn.args[0][0].match(/proxy compatibility/), "check your proxy warning emitted");
         });
     });
 });
