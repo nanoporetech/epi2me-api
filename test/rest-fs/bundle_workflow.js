92c41183aca223942ed1400d164ff1d4501eb8ff
Roger Pettett <rmp@psyphi.net>
2019-01-16 11:44:50 +0000

    full coverage for rest-fs

--- test/rest-fs/bundle_workflow.js
+++ test/rest-fs/bundle_workflow.js
@@ -1,28 +1,38 @@
-import REST from "../../lib/rest-fs";
-import utils from "../../lib/utils";
-
-const sinon  = require("sinon");
-const assert = require("assert");
-const bunyan = require("bunyan");
+import REST   from "../../lib/rest-fs";
+import utils  from "../../lib/utils";
+import sinon  from "sinon";
+import assert from "assert";
+import bunyan from "bunyan";
+import tmp    from "tmp";
 
 describe("rest-fs.bundle_workflow", () => {
+    let rest, log, ringbuf;
+
+    beforeEach(() => {
+	ringbuf = new bunyan.RingBuffer({ limit: 100 });
+        log     = bunyan.createLogger({ name: "log", stream: ringbuf });
+	rest    = new REST({
+	    log:   log,
+	    local: true,
+	    url:   tmp.dirSync().name,
+	});
+    });
+
     it("must invoke pipe with options", () => {
-	let ringbuf = new bunyan.RingBuffer({ limit: 100 });
-        let log     = bunyan.createLogger({ name: "log", stream: ringbuf });
-	let stub    = sinon.stub(utils, "_pipe").callsFake((uri, filepath, options, cb, progressCb) => {
-	    assert.deepEqual(options, { log: log }, "options passed");
+	let fake     = sinon.fake();
+	let progress = sinon.fake();
+	let stub     = sinon.stub(utils, "_pipe").callsFake((uri, filepath, options, cb, progressCb) => {
+	    assert.deepEqual(options, rest.options, "options passed");
 	    assert.equal(uri, "workflow/bundle/1234.tar.gz", "url passed");
 	    progressCb(0.5);
 	    progressCb(1);
 	    cb();
 	});
 
-	let fake = sinon.fake();
-	let progress = sinon.fake();
-	let rest = new REST({log: log});
 	assert.doesNotThrow(() => {
 	    rest.bundle_workflow("1234", "/path/to/1234", fake, progress);
 	});
+
 	assert(fake.calledOnce, "callback invoked");
 	assert(progress.calledTwice, "progress callback invoked");
 	stub.restore();
