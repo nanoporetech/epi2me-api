4f8d844cdb859e707b6a180788ad4c57ef8b10b9
Roger Pettett <rmp@psyphi.net>
2019-01-23 16:01:28 +0000

    promises, promises

--- test/epi2me/discoverQueue.js
+++ test/epi2me/discoverQueue.js
@@ -19,18 +19,19 @@ describe('epi2me.discoverQueue', () => {
 
     it('discovers successfully', async () => {
         let client = clientFactory();
-
-	let sqs = {
-	    getQueueUrl: sinon.fake((opts, callback) => {
-		assert.equal(opts.QueueName, "my_queue", "sqs getqueueurl for expected queue");
-		callback(null, {QueueUrl: "https://my.cloud/queues/my_queue"});
-	    }),
-	};
+	sinon.stub(client, "sessionedSQS").callsFake(() => {
+	    return {
+		getQueueUrl: () => {
+		    return {
+			promise: () => { return Promise.resolve({QueueUrl: "https://my.cloud/queues/my_queue"}); }
+		    }
+		}
+	    };
+	});
 
 	let data;
 	try {
-	    data = await client
-		.discoverQueue(sqs, 'my_queue');
+	    data = await client.discoverQueue('my_queue');
 	} catch (error) {
 	    assert.fail(error);
 	}
@@ -42,82 +43,45 @@ describe('epi2me.discoverQueue', () => {
         let client = clientFactory();
 	client.config.instance._discoverQueueCache["my_queue"] = "https://my.cloud/queues/my_queue";
 
-	let sqs = {
-	    getQueueUrl: sinon.fake((opts, callback) => {
-		assert.equal(opts.QueueName, "my_queue", "sqs getqueueurl for expected queue");
-		callback(null, {QueueUrl: "https://my.cloud/queues/my_queue"});
-	    }),
-	};
+	sinon.stub(client, "sessionedSQS").callsFake(() => {
+	    return {
+		getQueueUrl: () => {
+		    return {
+			promise: () => { return Promise.resolve({QueueUrl: "https://my.cloud/queues/my_queue"}); }
+		    }
+		}
+	    };
+	});
 
 	let data;
 	try {
-	    data = await client
-		.discoverQueue(sqs, 'my_queue');
+	    data = await client.discoverQueue('my_queue');
 	} catch (error) {
 	    assert.fail(error);
 	}
 
 	assert.equal(data, "https://my.cloud/queues/my_queue", "success callback fired with queue url");
-	sinon.assert.notCalled(sqs.getQueueUrl); // no need to run a real query
-    });
-
-    it('discovers badly', async () => {
-        let client = clientFactory();
-
-	let sqs = {
-	    getQueueUrl: sinon.fake((opts, callback) => {
-		assert.equal(opts.QueueName, "my_queue", "sqs getqueueurl for expected queue");
-		callback(null, {xQueueUrl: "https://my.cloud/queues/my_queue"});
-	    }),
-	};
-
-	try {
-	    await client
-		.discoverQueue(sqs, 'my_queue');
-	    assert.fail("unexpected success");
-	} catch (error) {
-	    assert.ok(error.match(/getqueueurl failure/), "failure callback fired with message");
-	};
+	sinon.assert.notCalled(client.sessionedSQS); // no need to run a real query
     });
 
     it('fails to discover', async () => {
         let client = clientFactory();
 
-	let sqs = {
-	    getQueueUrl: sinon.fake((opts, callback) => {
-		assert.equal(opts.QueueName, "my_queue", "sqs getqueueurl for expected queue");
-		callback(new Error("no such queue"));
-	    }),
-	};
+	sinon.stub(client, "sessionedSQS").callsFake(() => {
+	    return {
+		getQueueUrl: () => {
+		    return {
+			promise: () => { return Promise.reject(new Error("no such queue")); }
+		    }
+		}
+	    };
+	});
 
 	try {
-	    await client
-		.discoverQueue(sqs, 'my_queue');
+	    await client.discoverQueue('my_queue');
 	    assert.fail("unexpected success");
 	} catch (error) {
-	    assert.ok(error.match(/getqueueurl error/), "failure callback fired with message");
+	    assert.ok(String(error).match(/getqueueurl error/), "failure callback fired with message");
 	};
-    });
-
-    it('fails to discover with proxy set', async () => {
-        let client = clientFactory({proxy:"https://my.proxy:3128/"});
-
-	let sqs = {
-	    getQueueUrl: sinon.fake((opts, callback) => {
-		assert.equal(opts.QueueName, "my_queue", "sqs getqueueurl for expected queue");
-		callback(new Error("Unexpected close tag"));
-	    }),
-	};
-
-	try {
-	    await client
-		.discoverQueue(sqs, 'my_queue');
-	    assert.fail("unexpected success");
-
-	} catch (error) {
-	    assert.ok(error.match(/getqueueurl error/), "failure callback fired with message");
-	}
-
-	assert.ok(client.log.warn.args[0][0].match(/proxy compatibility/), "check your proxy warning emitted");
     });
 });
--- test/epi2me/uploadComplete.js
+++ test/epi2me/uploadComplete.js
@@ -1,26 +1,238 @@
-const assert = require("assert");
-const sinon  = require("sinon");
-
-import EPI2ME from "../../lib/epi2me";
+import assert    from "assert";
+import sinon     from "sinon";
+import bunyan    from "bunyan";
+import queue     from "queue-async";
+import fs        from "fs-extra";
+import { merge } from "lodash";
+import AWS       from "aws-sdk";
+import EPI2ME    from "../../lib/epi2me";
 
 describe('epi2me.uploadComplete', () => {
-    var client;
 
-    beforeEach(() => {
-        client = new EPI2ME();
-        sinon.stub(client, "sessionedSQS");
-        sinon.stub(client.log, "warn");
-        sinon.stub(client.log, "info");
-        sinon.stub(client, "sendMessage");
+    const clientFactory = (opts) => {
+	return new EPI2ME(merge({
+	    url: "https://epi2me-test.local",
+	    log: {
+		debug: sinon.stub(),
+		info:  sinon.stub(),
+		warn:  sinon.stub(),
+		error: sinon.stub(),
+	    }
+	}, opts));
+    };
+
+    it('sqs callback failure should handle error and log warning', async () => {
+	let client = clientFactory();
+	let sqs    = new AWS.SQS();
+
+	sinon.stub(client, "discoverQueue").resolves("http://my-queue/");
+	sinon.stub(client, "sessionedSQS").callsFake(() => { return sqs; });
+	sinon.stub(sqs,    "sendMessage").callsFake(() => {
+	    return {
+		promise: () => { return Promise.reject(new Error("uploadComplete failed")); }
+	    }
+	});
+
+	try {
+	    await client.uploadComplete("object-id", {id: "my-file"});
+	    assert.fail("unexpected success");
+	} catch (e) {
+	    assert.ok(String(e).match(/SQS sendmessage exception/));
+	}
     });
 
-    it('should handle error', (done) => {
-        let discoverQueue = sinon.stub(client, "discoverQueue").rejects(new Error("failed"));
+    it('sqs callback exception should handle error and log error', async () => {
+	let client = clientFactory();
+	let sqs    = new AWS.SQS();
+
+	sinon.stub(client, "discoverQueue").resolves("http://my-queue/");
+	sinon.stub(client, "sessionedSQS").callsFake(() => { return sqs; });
+	sinon.stub(sqs,    "sendMessage").callsFake(() => {
+	    return {
+		promise: () => { return Promise.reject(new Error("uploadComplete failed")); }
+	    }
+	});
+
+	try {
+	    await client.uploadComplete("object-id", {id: "my-file"});
+	} catch (e) {
+	    assert.fail(e);
+	}
+	assert(client.log.error.args[0][0].match(/uploadComplete failed/), "exception message logged");
+	assert(callback.args[0][0].match(/SQS sendmessage exception/), "error message passed back");
+    });
+
+    it('sqs callback success should move file and log info', () => {
+	let client = clientFactory();
+	let sqs    = new AWS.SQS();
+	let clock  = sinon.useFakeTimers();
+
+	sinon.stub(client, "discoverQueue").resolves("http://my-queue/");
+	sinon.stub(client, "sessionedSQS").callsFake(() => { return sqs; });
+	sinon.stub(sqs, "sendMessage").callsFake((obj, cb) => {
+	    assert.deepEqual(JSON.parse(obj.MessageBody),
+			     {"bucket":null,
+			      "outputQueue":null,
+			      "remote_addr":null,
+			      "user_defined":null,
+			      "utc":"1970-01-01T00:00:00.000Z",
+			      "path":"object-id",
+			      "prefix":"",
+			     },
+			     "uploadComplete payload");
+	    cb();
+	});
+	let callback = sinon.stub();
+	let moveUploadedFile = sinon.stub(client, "_moveUploadedFile").callsFake((file, cb) => {
+	    assert.deepEqual(file, {id: "my-file"}, "object metadata");
+	    cb();
+	});
 	assert.doesNotThrow(() => {
-            client.uploadComplete(null, 'item', () => {
-		sinon.assert.calledOnce(discoverQueue);
-		done();
-	    });
-	}, () => {});
+	    client.uploadComplete("object-id", {id: "my-file"}, callback);
+	});
+	assert(client.log.info.args[1][0].match(/my-file SQS message sent/), "info message logged");
+	clock.restore();
+    });
+
+    it('should handle chain info with exception', () => {
+	let client = clientFactory();
+	let sqs    = new AWS.SQS();
+
+	sinon.stub(client, "discoverQueue").resolves("http://my-queue/");
+	sinon.stub(client, "sessionedSQS").callsFake(() => { return sqs; });
+	sinon.stub(sqs, "sendMessage").callsFake((err, cb) => {
+	    cb();
+	});
+	let callback = sinon.stub();
+	let moveUploadedFile = sinon.stub(client, "_moveUploadedFile").callsFake((file, cb) => {
+	    cb();
+	});
+	client.config.instance.chain = {}; // components undefined
+	assert.doesNotThrow(() => {
+	    client.uploadComplete("object-id", {id: "my-file"}, callback);
+	});
+	assert(client.log.error.args[0][0].match(/exception parsing/), "exception message logged");
+	assert(callback.args[0][0].match(/json exception/), "error message passed back");
+    });
+
+    it('should handle & propagate additional metadata', () => {
+	let clock  = sinon.useFakeTimers();
+	let client = clientFactory();
+	let sqs    = new AWS.SQS();
+
+	sinon.stub(client, "discoverQueue").resolves("http://my-queue/");
+	sinon.stub(client, "sessionedSQS").callsFake(() => { return sqs; });
+	sinon.stub(sqs, "sendMessage").callsFake((obj, cb) => {
+	    assert.deepEqual(JSON.parse(obj.MessageBody),
+			     {"bucket":null,
+			      "outputQueue":null,
+			      "remote_addr":null,
+			      "user_defined":null,
+			      "utc":"1970-01-01T00:00:00.000Z",
+			      "path":"object-id",
+			      "prefix":"",
+			      "targetComponentId": 1,  // this
+			      "components": [],        // this
+			      "key_id": "data-secret", // this
+			      "agent_address": {       // this
+				  "city": "Cambridge",
+				  "ip": "127.0.0.1",
+			      }
+			     },
+			     "uploadComplete payload");
+	    cb();
+	});
+	let callback = sinon.stub();
+	let moveUploadedFile = sinon.stub(client, "_moveUploadedFile").callsFake((file, cb) => {
+	    cb();
+	});
+
+	client.config.instance.chain  = { components: [], targetComponentId: 1 };
+	client.config.instance.key_id = "data-secret";
+	client.config.options.agent_address = JSON.stringify({ "city": "Cambridge", "ip": "127.0.0.1" });
+	assert.doesNotThrow(() => {
+	    client.uploadComplete("object-id", {id: "my-file"}, callback);
+	});
+	assert(client.log.info.args[1][0].match(/my-file SQS message sent/), "info message logged");
+	clock.restore();
+    });
+
+    it('should handle bad agent_address', () => {
+	let clock  = sinon.useFakeTimers();
+	let client = clientFactory();
+	let sqs    = new AWS.SQS();
+
+	sinon.stub(client, "discoverQueue").resolves("http://my-queue/");
+	sinon.stub(client, "sessionedSQS").callsFake(() => { return sqs; });
+	sinon.stub(sqs, "sendMessage").callsFake((obj, cb) => {
+	    assert.deepEqual(JSON.parse(obj.MessageBody),
+			     {"bucket":null,
+			      "outputQueue":null,
+			      "remote_addr":null,
+			      "user_defined":null,
+			      "utc":"1970-01-01T00:00:00.000Z",
+			      "path":"object-id",
+			      "prefix":"",
+			      "targetComponentId": 1,  // this
+			      "components": [],        // this
+			      "key_id": "data-secret", // this
+			      "agent_address": {       // this
+				  "city": "Cambridge",
+				  "ip": "127.0.0.1",
+			      }
+			     },
+			     "uploadComplete payload");
+	    cb();
+	});
+	let callback = sinon.stub();
+	let moveUploadedFile = sinon.stub(client, "_moveUploadedFile").callsFake((file, cb) => {
+	    cb();
+	});
+
+	client.config.instance.chain  = { components: [], targetComponentId: 1 };
+	client.config.instance.key_id = "data-secret";
+	client.config.options.agent_address = "bad json data";
+	assert.doesNotThrow(() => {
+	    client.uploadComplete("object-id", {id: "my-file"}, callback);
+	});
+	assert(client.log.error.args[0][0].match(/Could not parse agent_address/), "error message logged");
+	clock.restore();
+    });
+
+    it('should inject upload & download message queues. is this used?', () => {
+	let clock  = sinon.useFakeTimers();
+	let client = clientFactory();
+	let sqs    = new AWS.SQS();
+
+	sinon.stub(client, "discoverQueue").resolves("http://my-queue/");
+	sinon.stub(client, "sessionedSQS").callsFake(() => { return sqs; });
+	sinon.stub(sqs, "sendMessage").callsFake((obj, cb) => {
+	    assert.deepEqual(JSON.parse(obj.MessageBody).components,
+			     [
+				 {id: 1, inputQueueName: "upload-q"},
+				 {id: 2, inputQueueName: "download-q"},
+			     ],
+			     "uploadComplete replaced component queue names");
+	    cb();
+	});
+	let callback = sinon.stub();
+	let moveUploadedFile = sinon.stub(client, "_moveUploadedFile").callsFake((file, cb) => {
+	    cb();
+	});
+
+	client.uploadMessageQueue = "upload-q";
+	client.downloadMessageQueue = "download-q";
+	client.config.instance.chain = {
+	    components: [
+		{id: 1, inputQueueName: "uploadMessageQueue"},
+		{id: 2, inputQueueName: "downloadMessageQueue"}
+	    ]
+	};
+
+	assert.doesNotThrow(() => {
+	    client.uploadComplete("object-id", {id: "my-file"}, callback);
+	});
+	assert(client.log.info.args[1][0].match(/my-file SQS message sent/), "info message logged");
+	clock.restore();
     });
 });
