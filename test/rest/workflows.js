a493732edc0615921ab31acaae99353f57657521
Roger Pettett <rmp@psyphi.net>
2019-01-24 11:53:36 +0000

    accounting for async autoConfigure

--- test/rest/workflows.js
+++ test/rest/workflows.js
@@ -1,54 +1,30 @@
-import REST from "../../lib/rest";
-import fs from "fs-extra";
+import fs        from "fs-extra";
+import sinon     from "sinon";
+import assert    from "assert";
+import bunyan    from "bunyan";
+import tmp       from "tmp";
+import path      from "path";
+import { merge } from "lodash";
+import REST      from "../../lib/rest";
 
-const sinon  = require("sinon");
-const assert = require("assert");
-const bunyan = require("bunyan");
-const tmp    = require("tmp");
-const path   = require("path");
+describe('rest.workflow', () => {
+    const restFactory = (opts) => {
+	let ringbuf = new bunyan.RingBuffer({ limit: 100 });
+        let log     = bunyan.createLogger({ name: "log", stream: ringbuf });
+	return new REST(merge({log: log}, opts));
+    };
 
-describe('rest.workflows', () => {
-    process.on('unhandledRejection', (reason, promise) => { console.log("[workflows.js] UNHANDLED PROMISE REJECTION", reason, promise); });
     it("must invoke list with options", () => {
-	let ringbuf    = new bunyan.RingBuffer({ limit: 100 });
-        let log        = bunyan.createLogger({ name: "log", stream: ringbuf });
-	let stub = sinon.stub(REST.prototype, "_list").callsFake((uri, cb) => {
+	let rest = restFactory();
+	let stub = sinon.stub(rest, "_list").callsFake((uri, cb) => {
 	    assert.equal(uri, "workflow", "url passed");
 	    cb();
 	});
 
 	let fake = sinon.fake();
-	let rest = new REST({log: log});
 	assert.doesNotThrow(() => {
 	    rest.workflows(fake);
 	});
 	assert(fake.calledOnce, "callback invoked");
-	stub.restore();
-    });
-
-    it("must list from filesystem", () => {
-	let ringbuf = new bunyan.RingBuffer({ limit: 100 });
-        let log     = bunyan.createLogger({ name: "log", stream: ringbuf });
-	let dir     = tmp.dirSync({unsafeCleanup: true}).name;
-
-	fs.mkdirpSync(path.join(dir, "workflows", "12345"));
-	fs.mkdirpSync(path.join(dir, "workflows", "34567"));
-
-	fs.writeFileSync(path.join(dir, "workflows", "12345", "workflow.json"), JSON.stringify({id_workflow: 12345}));
-	fs.writeFileSync(path.join(dir, "workflows", "34567", "workflow.json"), JSON.stringify({id_workflow: 34567}));
-
-	let rest = new REST({log: log, local: true, url: dir});
-	let fake = sinon.fake();
-	
-	new Promise((accept, reject) => {
-	    rest.workflows((err, data) => {
-		if(err) reject(fake(err));
-		accept(fake(null, data));
-	    });
-	})
-	    .then(() => {
-		sinon.assert.calledOnce(fake);
-		sinon.assert.calledWith(fake, null, [ { id_workflow: 12345 }, { id_workflow: 34567 }]);
-	    });
     });
 });
