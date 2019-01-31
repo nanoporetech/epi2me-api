c80d9ff21afbfebb483522788cbdb948346438c5
Glen Van Ginkel <glen.vanginkel@nanoporetech.com>
2019-01-31 13:07:36 +0000

    Add a demo of the EPI2ME core (web only) library

---
+++ demo.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const EPI2ME = require('./dist/web/index.js');
+
+const { profiles } = JSON.parse(fs.readFileSync(path.join(process.env.HOME, '.epi2me.json'), 'utf8'));
+const { apikey, apisecret } = Object.values(profiles).filter(p => p.endpoint.match(/epi2me.nanoporetech.com/g))[0];
+
+const client = new EPI2ME({ apikey, apisecret });
+
+client
+  .user()
+  .then(data => console.info('DATA:', data))
+  .catch(error => console.error('REJECT:', error));
