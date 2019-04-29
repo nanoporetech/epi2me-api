d6715424de72dc04ff26f6abd5e5cacbb75b20e4
Roger Pettett <rmp@psyphi.net>
2019-04-29 10:29:19 +0100

    roll in migration - too fiddly packaging something fairly insignificant (for now!)

--- src/db.js
+++ src/db.js
@@ -4,9 +4,29 @@ import path from 'path';
 
 export default class db {
   constructor(dbRoot) {
-    this.db = mkdirp(dbRoot).then(() => {
-      return sqlite.open(path.join(dbRoot, 'db.sqlite'), { Promise }).then(dbh => dbh.migrate()); // { force: 'last' }));
-    });
+    this.db = mkdirp(dbRoot)
+      .then(() => {
+        return sqlite.open(path.join(dbRoot, 'db.sqlite'), { Promise }).then(async dbh => {
+          try {
+            await Promise.all([
+              dbh
+                .run("CREATE TABLE meta (version CHAR(12) DEFAULT '' NOT NULL, id_workflow_instance INTEGER UNSIGNED)")
+                .then(() => {
+                  dbh.run("INSERT INTO meta (version) VALUES('0.0.1')");
+                }),
+              dbh.run("CREATE TABLE uploads (filename CHAR(255) DEFAULT '' NOT NULL PRIMARY KEY)"),
+              dbh.run("CREATE TABLE skips (filename CHAR(255) DEFAULT '' NOT NULL PRIMARY KEY)"),
+            ]);
+
+            return Promise.resolve(dbh);
+          } catch (e) {
+            return Promise.reject(e);
+          }
+        });
+      })
+      .catch(e => {
+        throw e;
+      });
   }
 
   async uploadFile(filename) {
