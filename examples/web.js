7b8fcefb6d4e384dd5e3e9e4a9afd6c22049323c
Roger Pettett <rmp@psyphi.net>
2019-08-22 12:01:24 +0100

    simplified read->json

--- examples/web.js
+++ examples/web.js
@@ -3,17 +3,12 @@ const path = require('path');
 
 const EPI2ME = require('../dist/web/index.js');
 
-const {
-  profiles
-} = JSON.parse(fs.readFileSync(path.join(process.env.HOME, '.epi2me.json'), 'utf8'));
-const {
-  apikey,
-  apisecret
-} = Object.values(profiles).filter(p => p.endpoint.match(/epi2me.nanoporetech.com/g))[0];
+const { profiles } = fs.readJSONSync(path.join(process.env.HOME, '.epi2me.json'), 'utf8');
+const { apikey, apisecret } = Object.values(profiles).filter(p => p.endpoint.match(/epi2me.nanoporetech.com/g))[0];
 
 const client = new EPI2ME({
   apikey,
-  apisecret
+  apisecret,
 });
 
 client
