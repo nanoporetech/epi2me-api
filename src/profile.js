fddedc684b5efc5673eeee3361390fd374f0f7d1
Roger Pettett <rmp@psyphi.net>
2019-08-22 17:31:46 +0100

    default endpoint handling

--- src/profile.js
+++ src/profile.js
@@ -2,14 +2,20 @@ import { homedir } from 'os';
 import fs from 'fs-extra';
 import path from 'path';
 import { merge } from 'lodash';
+import DEFAULTS from './default_options.json';
 
 export default class Profile {
   constructor(prefsFile) {
     this.prefsFile = prefsFile || Profile.profilePath();
     this.profileCache = {};
+    this.defaultEndpoint = process.env.METRICHOR || DEFAULTS.endpoint;
+
     try {
       const allProfiles = fs.readJSONSync(this.prefsFile);
       this.profileCache = merge(allProfiles.profiles, {});
+      if (allProfiles.endpoint) {
+        this.defaultEndpoint = allProfiles.endpoint;
+      }
     } catch (ignore) {
       // no file or corrupt file - ignore
     }
@@ -31,7 +37,12 @@ export default class Profile {
     }
 
     if (id) {
-      return this.profileCache[id] || {};
+      return merge(
+        {
+          endpoint: this.defaultEndpoint,
+        },
+        this.profileCache[id],
+      );
     }
 
     return {};
