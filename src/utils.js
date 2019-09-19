6480356762ee05913d762f5981bede8999d3617b
Javier Ferrer <jferrer@jferrer-mac.oxfordnanolabs.local>
2019-09-19 10:11:38 +0100

    MC-7297 extracted niceSize to its own file

--- src/utils.js
+++ src/utils.js
@@ -12,9 +12,6 @@ import { version as VERSION } from '../package.json';
 
 axios.defaults.validateStatus = status => status <= 504; // Reject only if the status code is greater than or equal to 500
 
-const UNITS = ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z'];
-const DIV = 1000;
-
 const utils = (function magic() {
   const internal = {
     sign: (req, optionsIn) => {
@@ -304,25 +301,6 @@ const utils = (function magic() {
         return Promise.reject(err);
       }
       return internal.responseHandler(res, options);
-    },
-
-    niceSize(sizeIn, unitidxIn) {
-      let unitidx = unitidxIn || 0;
-      let size = sizeIn || 0;
-
-      if (size > DIV) {
-        size /= DIV;
-        unitidx += 1;
-        if (unitidx >= UNITS.length) {
-          return '???';
-        }
-        return this.niceSize(size, unitidx);
-      }
-
-      if (unitidx === 0) {
-        return `${size}${UNITS[unitidx]}`;
-      }
-      return `${size.toFixed(1)}${UNITS[unitidx]}`;
     },
   };
 })();
