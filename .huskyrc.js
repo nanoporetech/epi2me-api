6d7fe618a0ee8f012b883893c0d9b3095fce2f70
Roger Pettett <rmp@psyphi.net>
2019-11-18 14:27:06 +0000

    yarn=>npm for huskyrc

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,7 @@ const tasks = arr => arr.join(' && ');
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks(['yarn test']),
-    'pre-push': tasks(['yarn clean', 'yarn test', 'yarn build:dist', 'git add dist --force']),
+    'pre-commit': tasks(['npm run test']),
+    'pre-push': tasks(['npm run clean', 'npm run test', 'npm run build:dist', 'git add dist --force']),
   },
 };
