f7bda4951fdb0cfaec9e583e3f4a5aada32e59a5
Roger Pettett <rmp@psyphi.net>
2020-01-09 18:27:20 +0000

    repaired web example for non-fs-y profile.js

--- examples/web.js
+++ examples/web.js
@@ -1,17 +1,18 @@
 const fs = require('fs-extra');
 const path = require('path');
+const EPI2ME = require('../dist/web');
 
-const EPI2ME = require('../dist/web/index.js');
-
-const { profiles } = fs.readJSONSync(path.join(process.env.HOME, '.epi2me.json'), 'utf8');
-const { apikey, apisecret } = Object.values(profiles).filter(p => p.endpoint.match(/epi2me.nanoporetech.com/g))[0];
+const profile = new EPI2ME.Profile(fs.readJSONSync(path.join(process.env.HOME, '.epi2me.json'), 'utf8'));
+const {
+  apikey,
+  apisecret
+} = profile.profile('production_signed');
 
 const client = new EPI2ME({
   apikey,
   apisecret,
 });
 
-client
-  .user()
+client.REST.user()
   .then(data => console.info('DATA:', data))
   .catch(error => console.error('REJECT:', error));
