334daa97b1cb7b300cd7e7ec385b8183d4660372
Roger Pettett <rmp@psyphi.net>
2020-03-11 14:08:11 +0000

    basic types

---
+++ src/niceSize.ts
@@ -0,0 +1,20 @@
+const niceSize = (sizeIn: number, unitIndexIn: number): string => {
+  const UNITS = ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z'];
+  const DIV = 1000;
+  let unitIndex = unitIndexIn || 0;
+  let size = sizeIn || 0;
+
+  if (size >= DIV) {
+    size /= DIV;
+    unitIndex += 1;
+
+    return unitIndex >= UNITS.length ? '???' : niceSize(size, unitIndex);
+  }
+
+  if (unitIndex === 0) {
+    return `${size}${UNITS[unitIndex]}`;
+  }
+  return `${size.toFixed(1)}${UNITS[unitIndex]}`;
+};
+
+export default niceSize;
