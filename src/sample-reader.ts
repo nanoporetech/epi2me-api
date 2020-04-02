dc6ae79cf6f00ee9ddd1e5a6c37c8c3ca2aecb56
Chris Ramshaw <chris.ramshaw@nanoporetech.com>
2020-04-02 12:17:45 +0100

    improve sample reader handling of bad paths

--- src/sample-reader.ts
+++ src/sample-reader.ts
@@ -51,9 +51,9 @@ export default class SampleReader implements Epi2meSampleReaderAPINS.ISampleRead
       }
       */
       const [experiment, sample] = takeRight(file.path.split(path.sep), 2);
-      const splitSample = sample.split('_');
-      const flowcell = takeRight(splitSample, 2)[0];
-      const [date, time] = splitSample.slice(0, 2);
+      const parser = /([0-9]{8})_([0-9]{4})_.*_(\w+\d+)_\w+/;
+      if (!parser.test(sample)) return;
+      const [, date, time, flowcell] = [...(sample.match(parser) || [])];
       const dateString = `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6, 8)}`;
       const timeString = `T${time.slice(0, 2)}:${time.slice(2, 4)}:00`;
       const startDate = new Date(dateString + timeString);
