7a4fece1c46345b83e2de3a60953ee9b69fe062f
Iain Shorter <iain.shorter@nanoporetech.com>
2020-07-28 15:57:33 +0100

    WIP: convert filestats to TS

---
+++ src/filestats.ts
@@ -0,0 +1,47 @@
+import path from 'path';
+import bytes from './filestats/default';
+import fasta from './filestats/fasta';
+import fastq from './filestats/fastq';
+import fastqgz from './filestats/fastqgz';
+
+const mapping = new Map([
+  ["fastq", fastq],
+  ["fasta", fasta],
+  ["fastqgz", fastqgz],
+  ["default", bytes],
+]);
+
+const DEFAULT_MAPPING = bytes;
+
+const longForm = new Map([
+  ['fq', 'fastq'],
+  ['fa', 'fasta'],
+]);
+
+type MappingOutput = { type: string; bytes: number; sequences: number }
+  | { type: string; bytes: number }
+  | { type: string; bytes: number; reads: number };
+
+export default function filestats(filePath: string): Promise<MappingOutput> {
+
+  let ext = path
+    .extname(filePath)
+    .toLowerCase()
+    .replace(/^[.]/, ''); // strip leading dot
+
+  const mappedForm = longForm.get(ext);
+  if (mappedForm) {
+    ext = mappedForm
+  }
+  if (ext === 'gz') {
+    const extArr = filePath.split('.').slice(1);
+    ext = extArr.reduce((prev, curr) => prev + (longForm.get(curr) ?? curr), '');
+  }
+
+  let mappingFunction = mapping.get(ext);
+  if (!mappingFunction) {
+    mappingFunction = DEFAULT_MAPPING;
+  }
+
+  return mappingFunction(filePath);
+}
