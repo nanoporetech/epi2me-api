9e08dbfe976dcc3aedcfe2ea19bb348a73464c68
Iain Shorter <iain.shorter@nanoporetech.com>
2020-08-04 09:30:08 +0100

    WIP: ensure loadUploadFiles test removes stub functions after testing

--- test/epi2me-fs/loadUploadFiles.js
+++ test/epi2me-fs/loadUploadFiles.js
@@ -39,8 +39,8 @@ describe('epi2me.loadUploadFiles', () => {
 
   it('should resolve with no work done if dirScanInProgress', async () => {
     const client = clientFactory();
-    sinon.stub(client, 'enqueueUploadFiles').resolves();
-    sinon.stub(utils, 'loadInputFiles').resolves();
+    stubs.push(sinon.stub(client, 'enqueueUploadFiles').resolves());
+    stubs.push(sinon.stub(utils, 'loadInputFiles').resolves());
 
     client.dirScanInProgress = true;
 
@@ -56,8 +56,8 @@ describe('epi2me.loadUploadFiles', () => {
 
   it('should do work and resolve if work to do', async () => {
     const client = clientFactory();
-    sinon.stub(client, 'enqueueUploadFiles').resolves();
-    sinon.stub(utils, 'loadInputFiles').resolves(['file-a.fastq', 'file-b.fastq']);
+    stubs.push(sinon.stub(client, 'enqueueUploadFiles').resolves());
+    stubs.push(sinon.stub(utils, 'loadInputFiles').resolves(['file-a.fastq', 'file-b.fastq']));
 
     client.inputBatchQueue = [];
     client.inputBatchQueue.remaining = 0;
@@ -80,8 +80,8 @@ describe('epi2me.loadUploadFiles', () => {
 
   it('should handle errors during loadInputFiles', async () => {
     const client = clientFactory();
-    sinon.stub(client, 'enqueueUploadFiles').resolves();
-    sinon.stub(utils, 'loadInputFiles').rejects(new Error('no such directory'));
+    stubs.push(sinon.stub(client, 'enqueueUploadFiles').resolves());
+    stubs.push(sinon.stub(utils, 'loadInputFiles').rejects(new Error('no such directory')));
 
     client.inputBatchQueue = [];
     client.inputBatchQueue.remaining = 0;
