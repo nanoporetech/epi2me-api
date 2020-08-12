eee160b0287241d56d8765eb4d08ccae3cb878a4
Iain Shorter <iain.shorter@nanoporetech.com>
2020-08-12 15:19:00 +0100

    export GQL as value instead of type from root

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,15 @@ import REST from './rest-fs';
 import SessionManager from './session-manager';
 import Profile from './profile-fs';
 import Factory from './factory';
+import GraphQL from './graphql';
 
 import type { UtilityFS } from './utils-fs';
-import type GraphQL from './graphql';
 import type { EPI2ME_OPTIONS as Options } from './epi2me-options';
 import type { Logger } from './Logger';
 import type * as graphQLSchema from './graphql-types';
 
-export { EPI2ME, REST, utils, SessionManager, Profile, Factory };
+export { EPI2ME, REST, GraphQL, utils, SessionManager, Profile, Factory };
 export const version = utils.version;
 export const EPI2ME_HOME = EPI2ME.EPI2ME_HOME;
 
-export type { UtilityFS, GraphQL, Options, Logger, graphQLSchema };
+export type { UtilityFS, Options, Logger, graphQLSchema };
