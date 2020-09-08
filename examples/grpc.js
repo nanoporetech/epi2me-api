5dacb425852aef05d734c835d5e14ff09a18fbe5
Chris Ramshaw <chris.ramshaw@nanoporetech.com>
2020-09-08 15:56:10 +0100

    add grpc example

--- examples/grpc.js
+++ examples/grpc.js
@@ -2,7 +2,7 @@ const { EPI2ME_RPC } = require('../dist/web');
 
 const jwt = process.env.JWT;
 
-const client = new EPI2ME_RPC('http://localhost:50051', jwt); // rename endpoint to url, normally done by epi2me
+const client = new EPI2ME_RPC('http://localhost:8080', jwt);
 
 client
   .healthCheck()
