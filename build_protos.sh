f32774ec2eddd386ee683a237bfd066c26204c93
Chris Ramshaw <chris.ramshaw@nanoporetech.com>
2020-09-15 11:01:07 +0100

    bundle protos so they can be found

--- build_protos.sh
+++ build_protos.sh
@@ -1,5 +1,10 @@
+mkdir protos
+
 protoc -I ./epi2me-protobufs/ \
     --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
     --js_out=import_style=commonjs,binary:./protos/ \
     --ts_out="service=grpc-web:./protos"/ \
     ./epi2me-protobufs/*.proto
+
+# mkdir -p ./dist/protos
+# cp -r ./protos ./dist/
