5e00e1e762477cf0b47a437df915b2bf59c4abaf
Chris Ramshaw <chris.ramshaw@nanoporetech.com>
2020-09-15 11:40:11 +0100

    add status api

--- src/grpc/index.ts
+++ src/grpc/index.ts
@@ -1,14 +1,17 @@
 import { grpc } from '@improbable-eng/grpc-web';
 import { WorkflowApi } from './api/workflow';
 import { SampleReaderApi } from './api/samples';
+import { StatusApi } from './api/status';
 
 export default class EPI2ME_RPC {
   public workflowApi: WorkflowApi;
   public samplesApi: SampleReaderApi;
+  public statusApi: StatusApi;
 
   constructor(public url: string, jwt: string, transport?: grpc.TransportFactory) {
     this.workflowApi = new WorkflowApi(url, jwt, transport);
     this.samplesApi = new SampleReaderApi(url, jwt, transport);
+    this.statusApi = new StatusApi(url, jwt, transport);
   }
 
   public close(): void {
