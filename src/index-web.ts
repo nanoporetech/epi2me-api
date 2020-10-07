ada439e9d66b5316cf58c5f2a20b585d694d60df
Iain Shorter <iain.shorter@nanoporetech.com>
2020-10-07 17:25:24 +0100

    remove: Sample type export from web version

--- src/index-web.ts
+++ src/index-web.ts
@@ -4,5 +4,4 @@ import EPI2ME_RPC from './grpc';
 export * as Helpers from './helpers';
 export { GraphQL, EPI2ME_RPC };
 
-export * from './sample.type';
 export * as graphQLSchema from './graphql-types';
