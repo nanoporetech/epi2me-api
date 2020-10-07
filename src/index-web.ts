fd64d5169590a58ce6e33836fbcedb7d8dc06466
Iain Shorter <iain.shorter@nanoporetech.com>
2020-10-07 16:30:53 +0100

    add: export GQL types and sample types in web builds

--- src/index-web.ts
+++ src/index-web.ts
@@ -3,3 +3,6 @@ import EPI2ME_RPC from './grpc';
 
 export * as Helpers from './helpers';
 export { GraphQL, EPI2ME_RPC };
+
+export * from './sample.type';
+export * as graphQLSchema from './graphql-types';
