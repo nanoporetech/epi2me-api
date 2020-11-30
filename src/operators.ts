1eb0c8e791b164c61e1080dffafed6e2b680aa3e
Iain Shorter <iain.shorter@nanoporetech.com>
2020-11-30 13:37:06 +0000

    doc: add example usage as comment for custom Observable operators

--- src/operators.ts
+++ src/operators.ts
@@ -4,6 +4,25 @@ import { Dictionary, isDefined } from 'ts-runtime-typecheck';
 import type { OperatorFunction } from 'rxjs';
 import type { Optional } from 'ts-runtime-typecheck';
 
+// PLEASE TEST ME
+
+/*
+  filterDefined
+    removes any nullish values from the stream
+
+  const source = new Subject
+  source.pipe(filterDefined).subscribe(console.log)  
+
+  source.next(12);
+  // 12
+  source.next();
+  source.next(null);
+  source.next('hello');
+  // 'hello'
+  source.next(undefined);
+  source.next(false);
+  // false
+*/
 export function filterDefined<T>(): OperatorFunction<Optional<T>, T> {
   return (source$: Observable<Optional<T>>): Observable<T> => {
     return new Observable(({ next, complete, error }) => {
@@ -20,7 +39,21 @@ export function filterDefined<T>(): OperatorFunction<Optional<T>, T> {
   };
 }
 
-// PLEASE TEST ME
+/*
+  recordDelta
+    compares the record with the previous record. only key values that have changed are emitted
+
+  const source = new Subject
+  source.pipe(recordDelta).subscribe(console.log)  
+
+  source.next({ a: 1, b: 'hello', c: false });
+  // { a: 1, b: 'hello', c: false }
+  source.next({ a: 2, b: 'hello', c: false });
+  // { a: 2 }
+  source.next({ a: 2, b: 'world', c: true });
+  // { b: 'world', c: true }
+
+*/
 export function recordDelta<V>(): OperatorFunction<Dictionary<V>, Dictionary<V>> {
   return (source$: Observable<Dictionary<V>>): Observable<Dictionary<V>> => {
     let previous: Dictionary<V> = {};
