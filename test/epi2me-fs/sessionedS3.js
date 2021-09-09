21c79de1e3d304b0003b02700addf3873630b6ba
Glen Van Ginkel <glen.vanginkel@nanoporetech.com>
2021-09-09 10:23:01 +0100

    Session management has been stripped out in favour of native AWS credentials refresh mechanism

--- test/epi2me-fs/sessionedS3.js
+++ test/epi2me-fs/sessionedS3.js
@@ -1,18 +1,12 @@
 import assert from 'assert';
-import sinon from 'sinon';
 import AWS from 'aws-sdk';
 import { EPI2ME_FS as EPI2ME } from '../../src/epi2me-fs';
 
 describe('epi2me.sessionedS3', () => {
   it('should session', () => {
     const client = new EPI2ME({});
-    client.sessionManager = {
-      session: () => {},
-    };
-    sinon.stub(client.sessionManager, 'session').resolves();
-
-    assert.doesNotThrow(async () => {
-      const s3 = await client.sessionedS3();
+    assert.doesNotThrow(() => {
+      const s3 = client.sessionedS3();
       assert.ok(s3 instanceof AWS.S3);
     });
   });
--- test/epi2me-fs/sessionedSQS.js
+++ test/epi2me-fs/sessionedSQS.js
@@ -1,5 +1,4 @@
 import assert from 'assert';
-import sinon from 'sinon';
 import AWS from 'aws-sdk';
 import { EPI2ME_FS as EPI2ME } from '../../src/epi2me-fs';
 
@@ -7,13 +6,8 @@ describe('epi2me.sessionedSQS', () => {
   it('should session', () => {
     const client = new EPI2ME({});
 
-    client.sessionManager = {
-      session: () => {},
-    };
-    sinon.stub(client.sessionManager, 'session').resolves();
-
-    assert.doesNotThrow(async () => {
-      const sqs = await client.sessionedSQS();
+    assert.doesNotThrow(() => {
+      const sqs = client.sessionedSQS();
       assert.ok(sqs instanceof AWS.SQS);
     });
   });
